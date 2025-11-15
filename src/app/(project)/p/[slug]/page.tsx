ff245d96cb22f95b90550a7f32c59df14cc51323
IrfanNha <mazirfan0099@gmail.com>
2025-11-15 13:11:39 +0700

    fix project single view

--- src/app/(project)/p/[slug]/page.tsx
+++ src/app/(project)/p/[slug]/page.tsx
@@ -265,7 +265,7 @@ export default async function ProjectDetailPage({ params }: PageParams) {
                   </div>
                 }
               >
-                <div className="rounded-xl md:border md:border-border/30 bg-background/90 p-0 md:p-10 shadow">
+                <div className="md:rounded-xl md:border md:border-border/30 md:bg-background/90 p-0 md:p-10 md:shadow">
                   <div
                     className="prose prose-lg prose-neutral max-w-none dark:prose-invert
                     [&>*+*]:mt-5
